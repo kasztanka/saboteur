823de69b3874f9a6cb9e1431876bb99fa9c28a0f
piotermiarer <piotr@atopik.pl>
2017-12-13 11:38:46 +0000

    Fix compilation errors

--- Board.h
+++ Board.h
@@ -6,8 +6,8 @@
 
 class Board {
 private:
-    const int ROWS = 6;
-    const int COLS = 8;
+    static const int ROWS = 6;
+    static const int COLS = 8;
     TunnelCard * cards[ROWS][COLS];
     TunnelCard * get_card(int x, int y);
     bool new_card_fits_to_neighbours(TunnelCard * new_card, int x, int y);
--- Game.h
+++ Game.h
@@ -1,5 +1,5 @@
-#ifndef SABOTEUR_SERVER_SABOTEURGAME_H
-#define SABOTEUR_SERVER_SABOTEURGAME_H
+#ifndef SABOTEUR_SERVER_GAME_H
+#define SABOTEUR_SERVER_GAME_H
 
 #include <vector>
 #include "Card.h"
@@ -10,15 +10,15 @@ using namespace std;
 class Client;
 
 class Game {
-    private:
-        vector<Client *> players;
-        vector<Card *> pile_of_cards;
-        Client * active_player;
-        Board * game_board;
-        void prepare_deck();
-    public:
-        Game();
-        Card * draw_card();
+private:
+    vector<Card *> pile_of_cards;
+    Client * active_player;
+    Board * game_board;
+    void prepare_deck();
+public:
+    vector<Client *> players;
+    Game();
+    Card * draw_card();
 };
 
 
